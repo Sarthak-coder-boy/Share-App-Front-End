40f5b261715e38b8662223ca52544a825b0ff525
Sarthak Gupta <81458354+Sarthak-coder-boy@users.noreply.github.com>
2023-02-04 21:46:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://share-file-site.herokuapp.com";
+const baseURL = "file-sharing-site.vercel.app";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
